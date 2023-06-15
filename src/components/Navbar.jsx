0456516c9f9c5519649998f70e6bf1526fe43c9d
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-16 05:06:29 +0530

    firebase database structure done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="logo">Chat Bee</span>
       <div className="user">
-        <img
-          src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-          alt=""
-        />
-        <span>John</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
