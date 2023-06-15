5a939399223f19618386b649c577447fe6c84280
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-15 09:03:05 +0530

    signup, signin form structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-function App() {
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./style.scss";
 
+function App() {
   return (
     <div>
-      Hello world!
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
